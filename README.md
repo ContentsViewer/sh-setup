2d349b7f91f01c729274d7571bf2c40b39314478
IOE <fivetwothreesix@gmail.com>
2020-06-02 17:46:34 +0900

    remove trash-cli from package list. refactoring

--- README.md
+++ README.md
@@ -2,6 +2,74 @@
 Setup Shell Environment with ONE Command. (Including WSL)
 
 ## Usage
+```sh
+# download scripts
+$ git clone https://github.com/ContentsViewer/sh-setup.git
+
+# move to script folder
+$ cd sh-setup
+
+# change the access mode of the script
+$ chmod 755 setup.sh
+
+# Now, let's start setup!
+$ ./setup.sh
+```
+
+if you are using wsl(windows subsystem for linux), 
+you can setup gui environment by adding option `--wsl`
+
+```sh
+$ ./setup.sh ---wsl
+```
+
+## Update
+You can re-run this script.
+
+At 2 or more, this script updates all packages.
+
+## Packages
+This script will add these packages.
+
+### build-essential
+### vim
+### git
+### tree
+### tmux
+At same time, add the `tmux.conf` by which you can use
+
+* mouse
+* scroll
+
+### htop
+### fzf
+### python-dev
+### pip
+### pipenv
+### zsh
+This script does not change current shell into zsh as default shell.
+
+You can launch zsh with command bellow.
+
+```sh
+zsh
+```
+
+### Optional
+if you flag the wsl option, this script will add these additional packages.
+
+#### xfce4-termianl
+#### xfce4
+install GUI Environment. for more detail, read the bellow section "How to Setup GUI Environment on WSL".
+
+#### font
+install fonts in windows into linux 
+#### fcitx
+
+## How to Setup GUI Environment on WSL
+1. First, you should install X Client on windows, such like "VcXsrv(free)" "X410".
+2. And Run this script added `--wsl`
+3. Finally, start gui environment `startxfce4`
 
 ## Author
 IOE <Github: ContentsViewer>
--- setup.sh
+++ setup.sh
@@ -13,202 +13,180 @@ usage="
 Usage: ${0##*/} [options]
 
 Options:
- --wsl       install gui environment for windows subsystem for Linux.
- -h, --help  display this help and exit
+ --wsl          install gui environment for windows subsystem for Linux.
+
+ -h, --help     display this help and exit
+ -V, --version  output version infromation and exit
 
 Version:
  $VERSION
 "
 
-# MY_DIR=$(pwd)
-# MY_DIR=$(dirname $0)
 MY_DIR=$(
   cd $(dirname $0)
   pwd
 )
+SRC_DIR='src'
 
-LF=$(printf '\\\n_')
-LF=${LF%_}
+NOTE="\033[34m[NOTE]\033[m   "
+WARNING="\033[33m[WARNING]\033[m"
+ERROR="\033[31m[ERROR]\033[m  "
 
-NOTE_LINE="\033[37;44m[NOTE] \033[m"
-WARNING_LINE="\033[37;43m[WARNING] \033[m"
-ERROR_LINE="\033[37;41m[ERROR] \033[m"
-
-print_usage_and_exit() {
-  echo "$usage" 1>&2
-  exit 1
+print_note() {
+  printf "$NOTE"
+  echo " $1"
 }
 
-error_exit() {
-  ${2+:} false && echo "${0##*/}: $2" 1>&2
-  exit $1
+print_warning() {
+  printf "$WARNING"
+  echo " $1"
+}
+
+print_error() {
+  printf "$ERROR"
+  echo " $1"
 }
 
 ######################################################################
 # Parse Arguments
 ######################################################################
-
 optwsl=0
-
 while [ $# -gt 0 ]; do
-  case $# in 0) break ;; esac
-
   case $1 in
     --wsl)
       optwsl=1
-      shift
-      continue
+      shift; continue
       ;;
-    --help | --version | -h) print_usage_and_exit ;;
-    *) print_usage_and_exit ;;
+    -h | --help)
+      echo "$usage"
+      exit
+      ;;
+    -V | --version)
+      echo "$version"
+      exit
+      ;;
+    *)
+      echo "$usage" 1>&2
+      exit 1
+      ;;
   esac
 done
 
 ######################################################################
 # Main Routine
 ######################################################################
-
-echo "$NOTE_LINE MY_DIR=$MY_DIR"
+print_note "MY_DIR=$MY_DIR"
 
 if [ "$(whoami)" = "root" ]; then
-  echo "$WARNING_LINE You now have root privilage."
-  echo "But this setup shell is designed under user privilage."
+  print_warning "You now have root privilage."
+  echo "But this script is designed under user privilage."
 
   while true; do
     yn=''
     printf "Are you sure to continue?[y/n]"
     read yn
     case $yn in
-      [Yy]) break ;;
-
-      [Nn]) exit 1 ;;
-
-      *) echo "please input [y/n]" ;;
+      [Yy]) break                        ;;
+      [Nn]) print_error 'Abort.'; exit 1 ;;
+      *) echo "please input [y/n]"       ;;
     esac
-
   done
 fi
 
-echo $NOTE_LINE 'Start setup'
+print_note 'Start Setup'
 sleep 1
 
 sudo apt update
 sudo apt upgrade
 
 # === Install Standard Apt ===========================================
-echo $NOTE_LINE 'Install build-essential'
+print_note 'Install build-essential'
 sudo apt install build-essential
 sleep 1
 
-echo $NOTE_LINE 'Install cmake'
+print_note 'Install cmake'
 sudo apt install cmake
 sleep 1
 
-echo $NOTE_LINE 'Install vim'
+print_note 'Install vim'
 sudo apt install vim
 sleep 1
 
-echo $NOTE_LINE 'Install git'
+print_note 'Install git'
 sudo apt install git
 sleep 1
 
-echo $NOTE_LINE 'Install tree'
+print_note 'Install tree'
 sudo apt install tree
 sleep 1
 
-echo $NOTE_LINE 'Install htop'
+print_note 'Install htop'
 sudo apt install htop
 sleep 1
 
 # === Install zsh ====================================================
-echo $NOTE_LINE 'Install zsh'
+print_note 'Install zsh'
 
 if type "zsh" >/dev/null 2>&1; then
-  echo "$NOTE_LINE zsh is already installed."
+  print_note 'zsh is already installed.'
 else
   sudo apt install zsh
 
-  $MY_DIR/setup_zsh.zsh
+  chomod 755 ${MY_DIR}/${SRC_DIR}/.setup.zsh
+  ${MY_DIR}/${SRC_DIR}/.setup.zsh
 
-  \cp -f $MY_DIR/.zshrc $HOME/.zshrc
-  \cp -f $MY_DIR/.zpreztorc $HOME/.zpreztorc
+  \cp -f ${MY_DIR}/${SRC_DIR}/.zshrc ${HOME}/.zshrc
+  \cp -f ${MY_DIR}/${SRC_DIR}/.zpreztorc ${HOME}/.zpreztorc
 fi
-
 sleep 1
 
 # === Install tmux ===================================================
-echo $NOTE_LINE 'Install tmux'
+print_note 'Install tmux'
 if type "tmux" >/dev/null 2>&1; then
-  echo "$NOTE_LINE tmux is already installed."
+  print_warning 'tmux is already installed.'
 else
   sudo apt install tmux
-  \cp -n $MY_DIR/.tmux.conf $HOME/.tmux.conf
+  \cp -n ${MY_DIR}/.tmux.conf ${HOME}/.tmux.conf
 fi
 sleep 1
 
 # === Install fzf ====================================================
-echo $NOTE_LINE 'Install fzf'
+print_note 'Install fzf'
 if type "fzf" >/dev/null 2>&1; then
-  echo "$NOTE_LINE fzf is already installed."
+  print_warning 'fzf is already installed.'
 else
-  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-  ~/.fzf/install
-
-  cat >>~/.fzf.bash <<-EOF
-
-export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
-export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
-
-	EOF
-
-  cat >>~/.fzf.zsh <<-EOF
+  git clone --depth 1 https://github.com/junegunn/fzf.git ${HOME}/.fzf
+  ${HOME}/.fzf/install
+  
+  fzf_conf=$(cat <<EOS
 
 export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
 export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
 
-	EOF
+EOS
+  )
+
+  echo "$fzf_conf" >>${HOME}/.fzf.bash
+  echo "$fzf_conf" >>${HOME}/.fzf.zsh
 fi
 sleep 1
 
 # === Install python environment =====================================
-echo $NOTE_LINE 'Install python-dev'
+print_note 'Install python-dev'
 sudo apt install python3-dev
 sudo apt install python-dev
-
 sleep 1
 
-# echo "$NOTE_LINE Install pyenv"
-# if type "pyenv" >/dev/null 2>&1; then
-#   echo "$NOTE_LINE pyenv is already installed."
-# else
-#   git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-
-#   if cat ~/.bashrc | grep '# pyenv setting. written by setup.sh'; then
-#     echo $WARNING_LINE 'pyenv setting is already written in .bashrc'
-#     echo $WARNING_LINE 'pyenv setting will not written in .bashrc'
-#   else
-#     echo '# pyenv setting. written by setup.sh' >>~/.bashrc
-#     echo 'export PYENV_ROOT="$HOME/.pyenv"' >>~/.bashrc
-#     echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >>~/.bashrc
-#     echo 'if command -v pyenv 1>/dev/null 2>&1; then' >>~/.bashrc
-#     echo '  eval "$(pyenv init -)"' >>~/.bashrc
-#     echo 'fi' >>~/.bashrc
-#   fi
-# fi
-
-# sleep 1
-
-echo $NOTE_LINE 'Install pip'
+print_note 'Install pip'
 if type "pip" >/dev/null 2>&1; then
-  echo "$NOTE_LINE pip is already installed."
+  print_warning 'pip is already installed.'
 else
-  echo add ~/.local/bin to PATH
-  # echo '# add ~/.local/bin to PATH (written by setup.sh)' >>~/.bashrc
-  echo 'export PATH=~/.local/bin:$PATH' >>~/.bashrc
-  PATH=~/.local/bin:$PATH
+  echo "add path'${HOME}/.local/bin' to PATH"
+  echo 'export PATH=${HOME}/.local/bin:$PATH' >>${HOME}/.bashrc
+  PATH=${HOME}/.local/bin:${PATH}
 
-  mkdir ~/.pip
-  cd ~/.pip
+  mkdir ${HOME}/.pip
+  cd ${HOME}/.pip
   wget "https://bootstrap.pypa.io/get-pip.py"
 
   python get-pip.py --user
@@ -216,67 +194,46 @@ else
 fi
 sleep 1
 
-# . ~/.bashrc
-
-echo $NOTE_LINE 'Install pipenv'
+print_note 'Install pipenv'
 pip install pipenv --user
-
-sleep 1
-
-# === Install trash ==================================================
-echo $NOTE_LINE 'Install trash-cli'
-if type "trash-list" >/dev/null 2>&1; then
-  echo "$NOTE_LINE trash-cli already installed."
-else
-  git clone https://github.com/andreafrancia/trash-cli.git ~/.trash-cli
-  cd ~/.trash-cli
-  sudo python setup.py install
-
-fi
 sleep 1
 
 # === WSL Option =====================================================
-case $optwsl in
-  0) : ;;
-  1)
-    echo "$NOTE_LINE wsl option is enabled."
+[ $optwsl -eq 1 ] && {
+  print_note 'WSL option is enabled.'
 
-    echo "$NOTE_LINE Install xfce4-terminal"
-    sudo apt install xfce4-terminal
-    sleep 1
+  print_note 'Install xfce4-terminal'
+  sudo apt install xfce4-terminal
+  sleep 1
 
-    echo "$NOTE_LINE Install xfce4-terminal"
-    sudo apt install xfce4
+  print_note 'Install xfce4'
+  sudo apt install xfce4
 
-    if cat ~/.bashrc | grep '# xfce4 setting. written by setup.sh'; then
-      echo $WARNING_LINE 'xfce4 setting is already written in .bashrc'
-
-    else
-      cat >>~/.bashrc <<-EOF
+  if cat ${HOME}/.bashrc | grep '# xfce4 setting. written by setup.sh'; then
+    print_warning 'xfce4 setting is already written in .bashrc'
+  else
+    cat >>${HOME}/.bashrc <<EOS
 
 # xfce4 setting. written by setup.sh
 export DISPLAY=:0.0
 export LIBGL_ALWAYS_INDIRECT=0
 
-			EOF
-    fi
+EOS
+  fi
+  sleep 1
 
-    sleep 1
+  print_note 'Install windows font'
+  sudo apt -y install fontconfig
+  sudo ln -s /mnt/c/Windows/Fonts /usr/share/fonts/windows
+  sudo fc-cache -fv
+  sleep 1
 
-    echo "$NOTE_LINE Install windows font"
-    sudo apt -y install fontconfig
-    sudo ln -s /mnt/c/Windows/Fonts /usr/share/fonts/windows
-    sudo fc-cache -fv
-    sleep 1
-
-    echo "$NOTE_LINE Install fcitx"
-    sudo apt -y install fcitx-mozc dbus-x11 x11-xserver-utils
-
-    if cat ~/.profile | grep '# fcitx setting. written by setup.sh'; then
-      echo $WARNING_LINE 'fcitx setting is already written in .profile'
-
-    else
-      cat >>~/.profile <<-EOF
+  print_note 'Install fcitx'
+  sudo apt -y install fcitx-mozc dbus-x11 x11-xserver-utils
+  if cat ${HOME}/.profile | grep '# fcitx setting. written by setup.sh'; then
+    print_warning 'fcitx setting is already written in .profile'
+  else
+    cat >>~/.profile <<EOS
 
 # fcitx setting. written by setup.sh
 
@@ -285,31 +242,15 @@ export QT_IM_MODULE=fcitx
 export XMODIFIERS="@im=fcitx"
 export DefaultIMModule=fcitx
 
-			EOF
-    fi
-    sleep 1
-    ;;
-esac
-
-# # === PATH settings ==================================================
-# echo $NOTE_LINE 'PATH settings'
-
-# if echo $PATH | grep -e ~/.local/bin >/dev/null 2>&1; then
-#   :
-# elif cat ~/.bashrc | grep '# add ~/.local/bin to PATH (written by setup.sh)' >/dev/null 2>&1; then
-#   :
-# else
-#   echo add ~/.local/bin to PATH
-#   echo '# add ~/.local/bin to PATH (written by setup.sh)' >>~/.bashrc
-#   echo 'export PATH=~/.local/bin:$PATH' >>~/.bashrc
-# fi
-
-# sleep 1
+EOS
+  fi
+  sleep 1
+}
 
 # === Final stage ====================================================
-echo $NOTE_LINE 'Final update...'
+print_note 'Final update...'
 sudo apt update
 sudo apt upgrade
 
-echo "$NOTE_LINE All process done!"
-echo "$NOTE_LINE Please restart shell environment."
+print_note 'All process done!'
+print_note 'Please restart shell environment.'
